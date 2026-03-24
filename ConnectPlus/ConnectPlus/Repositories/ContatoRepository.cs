3d2a916919bd8b15a424050f65c51e230847695f
MAYARA GUSSONATO DE OLIVEIRA SILVA <57039661837@info.local>
2026-03-24 16:54:21 -0300

    Nova Api

---
+++ ConnectPlus/ConnectPlus/Repositories/ContatoRepository.cs
@@ -0,0 +1,33 @@
+﻿using ConnectPlus.Models;
+using ContatoAPI.Interfaces;
+using System.Data.SqlClient;
+
+namespace ContatoAPI.Repositories;
+
+public class ContatoRepository : IContatoRepository
+{
+    public void Atualizar(Guid id, Contato contatoAtualizado)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Contato BuscarPorId(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Cadastrar(Contato novo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Deletar(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Contato> Listar()
+    {
+        throw new NotImplementedException();
+    }
+}
